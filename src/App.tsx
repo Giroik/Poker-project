13790ea7fac80ca9f327fa0dacb55081679e47ee
Alex Bondarev <Burn.The.Son@gmail.com>
2022-09-10 10:14:09 +0300

    исправил ошибку

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {getCard, getCardDeck} from "./lib/healpers/getCardDeck";
 import Card from "./components/Card";
 import Table from "./components/Table";
 import {useCrupie} from "./lib/crupie/useCrupie";
+import getCombination from "./lib/healpers/getCombination";
 
 function App() {
-    const [deck, setDeck] = useState(getCardDeck())
+    const [deck, setDeck] = useState<string[]>(getCardDeck())
+
+    const [table, setTable] = useState<any>([])
+
     const [hand, setHand] = useState<any>([])
-    //getCombination()
+    const [step, setStep] = useState<number>(1)
+    // getCombination()
     const buttonHandler = () => {
         let newCard = getCard(deck, setDeck)
 
         setHand([...hand, newCard])
     }
-    const crupieLoop = useCrupie(deck, setDeck)
+    const crupieLoop = useCrupie(deck, setDeck,table, setTable,step, setStep)
 
     return (
         <div className="App">
-            {/*<button onClick={buttonHandler}>Get card</button>*/}
+            <button onClick={buttonHandler}>Get card</button>
             <p>{hand.map((card: string) => <Card card={card}/>)}</p>
             <Table/>
-            {/*<button onClick={crupieLoop}>Crupie Loop</button>*/}
+            <button onClick={crupieLoop}>Crupie Loop</button>
         </div>
     );
 }
--- src/lib/crupie/useCrupie.ts
+++ src/lib/crupie/useCrupie.ts
@@ -1,79 +1,65 @@
 import {getCard, getCardDeck} from "../healpers/getCardDeck";
-import {useState} from "react";
+import {useMemo, useState} from "react";
 import {tab} from "@testing-library/user-event/dist/tab";
-const stepsName={
-    1:"Blinds",
-    2:"Preflop",
-    3:"Flop",
-    4:"Turn",
-    5:"River",
-    6:"Open"
+
+const stepsName = {
+    1: "Blinds",
+    2: "Preflop",
+    3: "Flop",
+    4: "Turn",
+    5: "River",
+    6: "Open"
 }
 
-function crupieLoop(step:any,setStep:any,hand1:any,table:any){
-    if(step===1)
-    {
+function crupieLoop({step, setStep, hand1, table, deck, setTable}: any) {
+    console.log(table)
+    if (step === 1) {
         console.log("Blinds")
         setStep(2)
-    }
-    else if(step===2)
-    {
+    } else if (step === 2) {
         console.log("Preflop")
         console.log(hand1)
         setStep(3)
-    }
-    else if(step===3)
-    {
+    } else if (step === 3) {
         console.log("Flop")
+        appendTable({step, setStep, deck, setTable})
         console.log(table)
         setStep(4)
-    }
-    else if(step===4)
-    {
+    } else if (step === 4) {
         console.log("Turn")
+        appendTable({step, setStep, deck, setTable})
         console.log(table)
         setStep(5)
-    }
-    else if(step===5)
-    {
+    } else if (step === 5) {
         console.log("River")
+        appendTable({step, setStep, deck, setTable})
         console.log(table)
         setStep(6)
-    }
-    else if(step===6)
-    {
+    } else if (step === 6) {
         console.log("Open")
         console.log(table)
     }
 
 }
-export function useCrupie(deck:any,setDeck:any)
-{
-    console.log("123")
-    // const [deck,setDeck]=useState<string[]>(getCardDeck())
-    //let card1=getCard(deck,setDeck)
-    let card2=getCard(deck,setDeck)
-    const [hand1,setHand1]=useState(["card1","card2"])
-    const [table,setTable]=useState<any>([])
-    const [step,setStep]=useState<number>(1)
-    /*if(table.length===0)
-    {
-        for(let i=0;i<3;i++)
-        {
-            setTable([...table,getCard(deck,setDeck)])
-        }
-    }
-    else if(table.length===3||table.length===4)
-    {
-        setTable([...table,getCard(deck,setDeck)])
-    }*/
 
-    return ()=>crupieLoop(step,setStep,hand1,table)
+function appendTable({table, setDeck, deck, setTable}: any) {
+    if (table.length === 0) {
+        for (let i = 0; i < 3; i++) {
+            setTable([...table, getCard(deck, setDeck)])
+        }
+    } else if (table.length === 3 || table.length === 4) {
+        setTable([...table, getCard(deck, setDeck)])
+    }
 }
 
+export function useCrupie(deck: any, setDeck: any, table: any, setTable: any, step: any, setStep: any) {
+    const card2 = useMemo(() => getCard(deck, setDeck), [])
+    const card1 = useMemo(() => getCard(deck, setDeck), [])
 
+    const [hand1, setHand1] = useState([card1, card2])
 
-
+    return () => crupieLoop({step, setStep, hand1, table, deck, setTable})
+}
 
 
 /*
